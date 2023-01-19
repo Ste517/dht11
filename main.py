213df7303590e1c992f3ad76abece1aed9c1bda2
Ste517 <66008197+Ste517@users.noreply.github.com>
2023-01-19 19:10:31 +0000

    v1

--- main.py
+++ main.py
@@ -8,20 +8,10 @@ with open("tokens.json", "r") as f:
 
 KEY=key_file["openweather"]
 city_id=key_file["openweatherid"]
-
+units="metric"
 
 # Crea l'URL per richiedere i dati meteorologici
-weather_url = f'http://api.openweathermap.org/data/2.5/weather?id={city_id}&appid={KEY}'
-
-# Effettua la richiesta e ottieni i dati
-response = requests.get(weather_url)
-
-# Converte i dati in formato JSON
-data = response.json()
-
-# Estrai la temperatura e l'umidità dai dati
-temperature = data['main']['temp']
-humidity = data['main']['humidity']
+weather_url = f'http://api.openweathermap.org/data/2.5/weather?id={city_id}&appid={KEY}&units={units}'
 
 app = Flask(__name__)
 app._static_folder = os.path.abspath("templates/static/")
@@ -37,12 +27,24 @@ def my_form_post():
     global temp
     global hum
     temp, hum = reqtemp, reqhum
+    temp = float(temp.replace("'",""))
+    hum = int(float(hum.replace("'","")))
     return request.form
 
 @app.route('/')
 def index():
+    # Effettua la richiesta e ottieni i dati
+    response = requests.get(weather_url)
+    
+    # Converte i dati in formato JSON
+    data = response.json()
+
+    # Estrai la temperatura e l'umidità dai dati
+    predtemp = data['main']['temp']
+    predhum = data['main']['humidity']
+
     # Passare i valori alla pagina HTML
-    return render_template('index.html', temp=temp, hum=hum)
+    return render_template('index.html', temp=temp, hum=hum, predtemp=predtemp, predhum=predhum)
 
 if __name__ == '__main__':
     app.run(debug=False)
